0f6451e56e9fe8fa03aadcd0a1455480956c0014
ltsai1 <louie.tsai@intel.com>
2019-06-03 16:31:31 -0700

    [Bug fix] fix Yarn cluster init issue, and by pass ssh verification

--- StartSparkCluster.py
+++ StartSparkCluster.py
@@ -26,44 +26,13 @@ def CSVwrite(filename,csvarray,fieldnames,ioflag,indexrow):
 	pass
     return ret
 
-def parseCSV(filename,csvfile):
-    dns_list=[]
-    ip_list=[]
-    fieldnames = ['NodeName', 'IP','DNS']
-    index=0
-    print(filename)
-    with open(filename) as f:
-            lines = f.readlines()
-    # get dns
-    for l in lines:
-        dns=l.split(' ')[1]
-        dns=dns.strip(' ')
-        dns=dns.strip('\n')
-        dns_list.append(dns)
-    # get ip and rows
-    for dns in dns_list:
-        print dns
-        addr = socket.getaddrinfo(dns,None)[0][4][0]
-        print addr
-        if index == 0:
-            nodename = 'master'
-        else:
-            nodename = 'slave'+str(index)
-        index+=1
-        row={fieldnames[0]:nodename , fieldnames[1]: addr,fieldnames[2]:dns}
-        print row
-        csv_rows.append(row)
-    csvfile = "spark_cluster.csv";
-    fieldnames = ['NodeName', 'IP','DNS']
-    CSVwrite(csvfile,'',fieldnames,'wb',True)
-    CSVwrite(csvfile,csv_rows,fieldnames,'ab',False)
-
 def createHostsfile():
     f = open("hosts", "w")
     for row in csv_rows:
         #scp -i "ai_tce.pem" scripts/instances.txt  ubuntu@ec2-18-191-197-117.us-east-2.compute.amazonaws.com:/home/ubuntu/
         # CSVFile
-        host = row['IP'] + "   " + row['NodeName']+"\n"
+        # AWS : need to use private ip instead of public ip for yarn cluster
+        host = row['PrivateIP'] + "   " + row['NodeName']+"\n"
         f.write(host)
     f.close()
 
@@ -145,8 +114,7 @@ def Ops_SCP(dst,filename):
     p = subprocess.Popen(cmd)
     sts = os.waitpid(p.pid, 0)
 
-def Ops_generate_scp_script(row,dst,filename):
-    # ssh -o ProxyCommand='nc -x proxy-us.intel.com:1080 %h %p' -L 8888:localhost:8888 -L 8080:localhost:8080  -i "ai_tce.pem" ubuntu@$1
+def Ops_generate_scp_script(dst,filename,action):
     if os.path.isfile("./tmp/scp.sh"):
           os.remove("./tmp/scp.sh")
     if Need_Intel_Proxy==True:
@@ -157,7 +125,10 @@ def Ops_generate_scp_script(row,dst,filename):
     # Create connect scripts
     if os.path.isdir('./tmp')==False:
         os.mkdir("./tmp")
-    scp_cmd ="scp "+intel_proxy_cmd+" "+"  -i ../ai_tce.pem "+"ubuntu@"+row['DNS']+ " "+filename+" "+dst
+    if action == 'push':
+        scp_cmd ="scp "+intel_proxy_cmd+" "+"  -i ../ai_tce.pem "+filename+" "+dst
+    elif action == "pull":
+        scp_cmd ="scp "+intel_proxy_cmd+" "+"  -i ../ai_tce.pem "+dst+"  "+filename
     filepath = "./tmp/scp.sh"
     f = open(filepath, "w")
     f.write(scp_cmd)
@@ -165,14 +136,17 @@ def Ops_generate_scp_script(row,dst,filename):
     os.chmod(filepath, stat.S_IRUSR |stat.S_IEXEC |stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH )
     return filepath
 
-def Ops_generate_ssh_script(row):
+def Ops_generate_ssh_script(row,cluster):
     # ssh -o ProxyCommand='nc -x proxy-us.intel.com:1080 %h %p' -L 8888:localhost:8888 -L 8080:localhost:8080  -i "ai_tce.pem" ubuntu@$1
     if Need_Intel_Proxy==True:
         intel_proxy_cmd = "-o ProxyCommand='nc -x proxy-us.intel.com:1080 %h %p'"
     else:
         intel_proxy_cmd = ''
     if row['NodeName'] == "master":
-        port_tunnel = "-L 8888:localhost:8888 -L 8080:localhost:8080 -L 8088:localhost:8088"
+        if cluster == "standalone":
+            port_tunnel = "-L 8888:localhost:8888 -L 8080:localhost:8080 "
+        elif cluster == "yarn":
+            port_tunnel = "-L 8888:localhost:8888 -L 8088:localhost:8088"
     else:
         port_tunnel = ''
 
@@ -188,9 +162,82 @@ def Ops_generate_ssh_script(row):
     os.chmod(filepath, stat.S_IRUSR |stat.S_IEXEC |stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH )
     return filepath
 
+def getPrivateAddr(dns):
+    IP='0.0.0.0'
+    # get /etc/hostname
+    dst = "ubuntu@"+dns+":/etc/hostname"
+    filepath = "."
+    filepath = Ops_generate_scp_script(dst,filepath,"pull")
+    try:
+        os.system(filepath)
+    except:
+        print "ERROR : NO VALID DNS!"
+        return IP
+    # parse IP from hostname
+    with open("./hostname") as f:
+            lines = f.readlines()
+    for l in lines:
+        l=l.strip("\n")
+        print l
+        IP=l.split("-")[1]+'.'+l.split("-")[2]+'.'+l.split("-")[3]+'.'+l.split("-")[4]
+        print IP
+
+    return IP
+
+def parseCSV(filename,csvfile):
+    dns_list=[]
+    ip_list=[]
+    fieldnames = ['NodeName', 'IP','DNS','PrivateIP']
+    index=0
+    print(filename)
+    with open(filename) as f:
+            lines = f.readlines()
+    # get dns
+    for l in lines:
+        dns=l.split(' ')[1]
+        dns=dns.strip(' ')
+        dns=dns.strip('\n')
+        dns_list.append(dns)
+    # get ip and rows
+    for dns in dns_list:
+        print dns
+        addr = socket.getaddrinfo(dns,None)[0][4][0]
+        print addr
+        if index == 0:
+            nodename = 'master'
+        else:
+            nodename = 'slave'+str(index)
+        private_addr = getPrivateAddr(dns)
+        index+=1
+        row={fieldnames[0]:nodename , fieldnames[1]:addr,fieldnames[2]:dns,fieldnames[3]:private_addr}
+        print row
+        csv_rows.append(row)
+    csvfile = "spark_cluster.csv";
+    fieldnames = ['NodeName', 'IP','DNS','PrivateIP']
+    CSVwrite(csvfile,'',fieldnames,'wb',True)
+    CSVwrite(csvfile,csv_rows,fieldnames,'ab',False)
+
 def main(filename,cluster,csvfile):
     print 'main'
-    parseCSV(filename,csvfile)
+    # read from CSV file if input is csv file
+    if filename == csvfile:
+        print "using exist CSV file"
+	import csv
+        with open(csvfile, 'rb') as f:
+                reader = csv.reader(f)
+                index = 0
+                fieldnames = []
+                csvrow={}
+                for row in reader:
+                    if index == 0 :
+                        fieldnames = row
+                    else:
+                        csvrow={fieldnames[0]:row[0] , fieldnames[1]:row[1],fieldnames[2]:row[2],fieldnames[3]:row[3]}
+                        csv_rows.append(csvrow)
+                    index+=1
+    else:
+        parseCSV(filename,csvfile)
+
     createHostsfile()
     for row in csv_rows:
         if row['NodeName']== 'master':
@@ -199,9 +246,8 @@ def main(filename,cluster,csvfile):
                 # /home/ubuntu/install_files/hadoop-2.7.7/etc/hadoop/slaves
                 createSlavesfile()
                 dst ="ubuntu@"+row['DNS']+":/home/ubuntu/install_files/hadoop-2.7.7/etc/hadoop/"
-                filepath = Ops_generate_scp_script(row,dst,"slaves")
+                filepath = Ops_generate_scp_script(dst,"slaves","push")
                 os.system(filepath)
-
     gnome_terminal_cmd = ["gnome-terminal"]
     for row in csv_rows:
         # SCP the config file
@@ -209,32 +255,38 @@ def main(filename,cluster,csvfile):
 
         # CSVFile
         dst = "ubuntu@"+row['DNS']+":/home/ubuntu/Scripts"
-        filepath = Ops_generate_scp_script(row,dst,csvfile)
+        filepath = Ops_generate_scp_script(dst,csvfile,"push")
         os.system(filepath)
 
         # /etc/hosts
         dst = "ubuntu@"+row['DNS']+":/home/ubuntu/Scripts"
-        filepath = Ops_generate_scp_script(row,dst,"hosts")
+        filepath = Ops_generate_scp_script(dst,"hosts","push")
         os.system(filepath)
 
         # /etc/hostname
         createHostnamefile(row)
-        filepath = Ops_generate_scp_script(row,dst,"hostname")
+        filepath = Ops_generate_scp_script(dst,"hostname","push")
         os.system(filepath)
 
         # create init.sh
         createInitScript(row,cluster,master_dns)
-        filepath = Ops_generate_scp_script(row,dst,"init.sh")
+        filepath = Ops_generate_scp_script(dst,"init.sh","push")
+        os.system(filepath)
+
+        #~/.ssh/config
+        dst = "ubuntu@"+row['DNS']+":/home/ubuntu/.ssh"
+        filepath = Ops_generate_scp_script(dst,"config","push")
         os.system(filepath)
 
         # compose gnome-terminal command
-        filepath = Ops_generate_ssh_script(row)
+        filepath = Ops_generate_ssh_script(row,cluster)
 
         #gnome-terminal  --tab -e "bash -c 'ps -ef; bash'" --tab -e "bash -c 'ls;bash'" --tab -e "bash -c 'top -n 1; bash'"
         gnome_terminal_cmd.append("--tab")
         gnome_terminal_cmd.append("-e")
         bashcmd = "bash -c '"+filepath+"';bash"
         gnome_terminal_cmd.append(bashcmd)
+        #raw_input("Press Enter to continue...")
 
     print gnome_terminal_cmd
     p = subprocess.Popen(gnome_terminal_cmd)
@@ -245,7 +297,7 @@ def main(filename,cluster,csvfile):
     for row in csv_rows:
         # create stop.sh
         createStopScript(row,cluster)
-        filepath = Ops_generate_scp_script(row,dst,"stop.sh")
+        filepath = Ops_generate_scp_script(dst,"stop.sh","push")
         os.system(filepath)
         time.sleep( 1 )
 
